7dc1aa580b07b58119b5b3c72be9e743fbf76977
GuillaumeVerlodt <guillaume.verlodt@student.hogent.be>
2024-10-14 10:06:58 +0200

    Update eslint.config.js with new rules

--- webapp/eslint.config.js
+++ webapp/eslint.config.js
@@ -1,31 +1,29 @@
-import eslintPlugin from '@eslint/js';
-const { eslintrc } = eslintPlugin;
-
 export default [
-  eslintrc(),
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-      globals: {
-        es6: true,
-        es2021: true,
-        node: true,
-        mocha: true,
+    {
+      files: ['**/*.js'],
+      languageOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        globals: {
+          // Definieer globale variabelen zoals eerder
+          es6: true,
+          es2021: true,
+          node: true,
+          mocha: true,
+        },
+      },
+      rules: {
+        'indent': ['error', 2, { SwitchCase: 1 }],
+        'linebreak-style': ['error', 'unix'],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always'],
+        'comma-dangle': ['error', 'always-multiline'],
+        'no-tabs': ['error'],
+        'max-len': ['error', { code: 120, tabWidth: 2 }],
+        'arrow-parens': ['error', 'always'],
+        'brace-style': ['error', '1tbs', { allowSingleLine: false }],
+        'no-inner-declarations': 'off',
       },
     },
-    rules: {
-      'indent': ['error', 2, { SwitchCase: 1 }],
-      'linebreak-style': ['error', 'unix'],
-      'quotes': ['error', 'single'],
-      'semi': ['error', 'always'],
-      'comma-dangle': ['error', 'always-multiline'],
-      'no-tabs': ['error'],
-      'max-len': ['error', { code: 120, tabWidth: 2 }],
-      'arrow-parens': ['error', 'always'],
-      'brace-style': ['error', '1tbs', { allowSingleLine: false }],
-      'no-inner-declarations': 'off',
-    },
-  },
-];
+  ];
+  
